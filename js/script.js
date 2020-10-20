0bdf52f217746b835f28640ed825813bb7ff58ce
AZC <albertzcheng@gmail.com>
2020-10-19 17:56:20 -0700

    draft of the different stages in html and the pseudocode for javascript

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Stage 1:  Cover page with button that starts the program
+// Once the program starts, the timer starts 
+//Stage 2:  Questions and answer program - goes through each of the question and prompt the user to select answers
+// The timer stops either when the user has answered all the questions or when it goes into zero 
+//Stage 3:  Once all the questions are answered or the timer runs out, we prompt the user for initials for high score
+//Stage 4:  If the user makes it to the high score, it is displayed.  Otherwise, a message says the user didn't not get to a highscore
+//There are 2 buttons for this stage, either you start again or you can clear the top scores
